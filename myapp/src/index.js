dc2911b3b56c88c778a1ac731c66be9b7727ada3
Виктория <viktoria.servetnik@yandex.ru>
2021-05-13 15:46:51 +0300

    Начали создавать

---
+++ myapp/src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+
+import Header from "./components/Header";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer";
+import ArrowTop from "./components/ArrowTop";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header />
+    <Main />
+    <Footer />
+    <ArrowTop />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+document.querySelector(".ArrowTop").addEventListener("click", () => {
+  window.scrollTo(0, 0);
+})
+
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 300) {
+      document.querySelector(".ArrowTop").classList.add("animate_zoomIn");
+      document.querySelector(".ArrowTop").classList.remove("hidden");
+    }
+    else  {
+      document.querySelector(".ArrowTop").classList.remove("animate_zoomIn");
+      document.querySelector(".ArrowTop").classList.add("hidden");
+
+      
+    }
+})
+
